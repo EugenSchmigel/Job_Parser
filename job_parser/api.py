6466a306e132e0bec422cdb224747c48d6a2ac79
Eugen <eugenschmigel@gmail.com>
2023-09-13 00:08:50 +0200

    added abstract class "Abstract_Job_API"

--- job_parser/api.py
+++ job_parser/api.py
@@ -1,2 +1,12 @@
 import requests
 from abc import ABC, abstractmethod
+
+class Abstract_Job_API(ABC):
+    """Абстрактный класс"""
+    def __init__(self, url):
+        self.url = url
+
+    @abstractmethod
+    def get_vacancies(self, search_request):
+        """Абстрактный метод получить вакансий"""
+        pass
