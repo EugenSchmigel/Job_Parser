254e5f9db519f925b60817d4932835a682918d47
Eugen <eugenschmigel@gmail.com>
2023-09-13 00:12:01 +0200

    added class "Vacancy" and init method

--- job_parser/vacancy.py
+++ job_parser/vacancy.py
@@ -1,1 +1,11 @@
 import json
+
+class Vacancy:
+    """Класс вакансии"""
+    def __init__(self, title, url, salary, description, salary_currency):
+
+        self.title = title
+        self.url = url
+        self.salary = salary
+        self.description = description
+        self.salary_currency = salary_currency
